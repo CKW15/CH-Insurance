6be3fa6e8026e142f57df43c6e533c10d349e541
Connor Wilson <c.kakenga.w@gmail.com>
2024-01-25 22:45:45 +1300

    Fixed the issue with the menu not looping - Added policy type description for case 1 that reads the descriptions from a txt file. - Also added a create user function that saves new user to users.txt - Can you create the login function and main menu for admin and I'll do the main menu for the user.

--- CH Insurance App/CH Insurance App.cpp
+++ CH Insurance App/CH Insurance App.cpp
@@ -1,9 +1,52 @@
+// -------------------------------- BLANK  -------------------------
+// ------------------------------------ END ------------------------
+
 // -------------------------- CH INSURANCE APP -------------------------------
 #include<iostream>
+#include <fstream>
+#include <string>
+#include <vector> 
 
 using namespace std;
 
+// ------------------------ USER CLASS ATTRIBUTES ------------------------------
+class User {
+public:
+    string username;
+    string password;
+    bool isAdmin;
 
+    User(const string& uname, const string& pwd, bool admin)
+        : username(uname), password(pwd), isAdmin(admin) {}
+};
+// ----------------------------- END ---------------------------------------------
+
+
+
+
+// -------------------------------- VIEW POLICY DESCRIPTIONS -------------------------
+void policyInfo() {
+    cout << "\n          Available Insurance Policies " << endl;
+    cout << "            ---------------------------- " << endl;
+    cout << endl;
+    ifstream inputFile("policy-desc.txt");
+
+    if (!inputFile.is_open()) {
+        cout << " Error opening policy description file." << endl;
+        return;
+    }
+
+    // Read the file line by line
+    string line;
+    while (getline(inputFile, line)) {
+        // Process each line as needed
+        cout << line << endl;
+    }
+
+    // Close the file
+    inputFile.close();
+}
+// ------------------------------------ END ------------------------------------------
 
 // -------------------------------- USER INPUT FOR MAIN MENU  ----------------
 int getUserChoice() {
@@ -30,18 +73,68 @@ int getUserChoice() {
 // ------------------------------------ END ----------------------------------
 
 
+// -------------------- SAVES USER LOGIN TO : .txt FILE  --------------------------
+void saveUsers(const vector<User>& users) {
+    ofstream file("users.txt", ios::app);  // Open file in append mode
+
+    for (const auto& user : users) {
+        file << user.username << ' ' << user.password << ' ' << user.isAdmin << endl;
+    }
+}
+
+//---------------------------------- END --------------------------------------------
+
+
+//  ------------------------------------ CREATE ACCOUNT FUNCTION -----------------------------------------------
+User createAccount(vector<User>& users) {
+    string uname, pwd, confirmPwd;
+    bool admin = false;
+
+    cout << " Enter new username: ";
+    cin >> uname;
+    cout << "\n" << endl;
+
+    // Ask for the password and its confirmation
+    do {
+        cout << " Enter new password: ";
+        cin >> pwd;
+        cout << "\n" << endl;
+
+        cout << " Confirm password: ";
+        cin >> confirmPwd;
+        cout << "\n" << endl;
+
+        if (pwd != confirmPwd) {
+            cout << " Passwords do not match. Please try again." << endl;
+        }
+        else if (pwd == uname) {
+            cout << " Password cannot be the same as the username. Please choose a different password." << endl;
+        }
+    } while (pwd != confirmPwd || pwd == uname);
+
+
+    users.emplace_back(uname, pwd, admin);
+
+    // Save the new user immediately
+    saveUsers(users);
+
+    cout << " Account created successfully." << endl;
+    cout << "\n" << endl;
+    return users.back();
+}
+//------------------------------------------- END -------------------------------------------------------------
+
+
 
 // ----------------------------- DISPLAYS MAIN MENU  -------------------------
 void displayMainMenu() {
-
-    int choice;
-
-        cout << " **** CH Insurance ****" << endl;
-        cout << " ---------------------- " << endl;
-        cout << " 1. View Available Policies" << endl;
-        cout << " 2. Create Account" << endl;
-        cout << " 3. Login" << endl;
-        cout << " 4. Exit" << endl;    
+   
+    cout << "\n **** CH Insurance ****" << endl;
+    cout << "\n ---------------------- \n" << endl;
+    cout << "\n 1. View Available Policies" << endl;
+    cout << "\n 2. Create Account" << endl;
+    cout << "\n 3. Login" << endl;
+    cout << "\n 4. Exit" << endl;
         
     }
 // ------------------------------- END ------------------------------------------------------
@@ -50,26 +143,30 @@ void displayMainMenu() {
 
 // ------------------------------------- INT MAIN ---------------------------------------------------------------------
 int main() {
-
+    vector<User> users;
     int choice;
+    do {
 
-    displayMainMenu();
+      displayMainMenu();
 
-    choice = getUserChoice();
+      choice = getUserChoice();
 
-    switch (choice) {
-    case 1:
-        cout << "test" << endl;
-        break;
-    case 2:
-        cout << "test2" << endl;
-        break;
-    case 3:
-        cout << "test3" << endl;
-        break;
-    case 4:
-        cout << "test4" << endl;
-    } while (choice != 4);
+      switch (choice) {
+      case 1:
+          policyInfo();
+          cout << endl;
+          break;
+      case 2:
+          createAccount(users);
+          break;
+      case 3:
+          cout << "test3" << endl;
+          break;
+      case 4:
+          cout << "test4" << endl;
+          break;
+      }
+    } while (choice != 4);;
 
     return 0;
 }
