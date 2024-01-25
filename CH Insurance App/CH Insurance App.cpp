6d3d093b241bd15b5852606f7ccf4ac853bc7ef9
Harlen <Harlenmurray@hotmail.com>
2024-01-25 22:26:39 +1300

    Changed Main menu display to void function -

--- CH Insurance App/CH Insurance App.cpp
+++ CH Insurance App/CH Insurance App.cpp
@@ -1,1 +1,24 @@
 // CH Insurance 
+
+#include<iostream>
+
+using namespace std;
+
+
+void displayMainMenu() {
+
+        cout << " **** CH Insurance ****" << endl;
+        cout << " ---------------------- " << endl;
+        cout << " 1. View Available Policies" << endl;
+        cout << " 2. Create Account" << endl;
+        cout << " 3. Login" << endl;
+        cout << " 4. Exit" << endl;
+     
+    }
+
+
+    int main() {
+
+        displayMainMenu();
+
+    }
